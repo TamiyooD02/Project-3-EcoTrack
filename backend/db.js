2e73bde1ba0e61a6491320ee2386da6c3661de67
tamiyood <desir.t@northeastern.edu>
2026-03-18 17:31:29 -0400

    added eslint and prettier

--- backend/db.js
+++ backend/db.js
@@ -1,28 +1,28 @@
 //import mongodb library
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
 //load the .env file
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 //connection string
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
 
 //create new mongodb client
-const client = new MongoClient(uri)
+const client = new MongoClient(uri);
 
 //connect to mongodb and export the database
-let db
+let db;
 
 async function connectDB() {
-    await client.connect()
-    db = client.db('ecotrack')
-    console.log('Connected to MongoDB')
+  await client.connect();
+  db = client.db('ecotrack');
+  console.log('Connected to MongoDB');
 }
 
 // function that gets database from other files
 function getDB() {
-    return db
+  return db;
 }
 
-export { connectDB, getDB }
+export { connectDB, getDB };
--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
 //import dotenv
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 //import express library
-import express from 'express'
+import express from 'express';
 
 //import database connection function
-import { connectDB } from './db.js'
-import activitiesRouter from './routes/activities.js'
+import { connectDB } from './db.js';
+import activitiesRouter from './routes/activities.js';
 
 //create server app
-const app = express()
+const app = express();
 
 //lets the server read JSON data from frontend
-app.use(express.json())
+app.use(express.json());
 
 //connect activities routes
-app.use('/api/activities', activitiesRouter)
+app.use('/api/activities', activitiesRouter);
 
 //test route to see if server is working
 app.get('/api/ping', (req, res) => {
-    res.json({ok: true})
-})
+  res.json({ ok: true });
+});
 
 //connect to mongodb first then start the server
-connectDB() .then(() => {
-    app.listen(3001, () => {
-        console.log('Server running on port 3001')
-    })
-})
+connectDB().then(() => {
+  app.listen(3001, () => {
+    console.log('Server running on port 3001');
+  });
+});
